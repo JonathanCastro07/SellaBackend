5a8203c02e457acd5f046711d0fb3373f4590c9f
jonathan <jmanjarres238@gmail.com>
2026-07-13 16:22:59 -0500

    Agregar dominio de Vercel a CORS

--- src/main/java/com/JonathanCastro07/Sella/config/SecurityConfig.java
+++ src/main/java/com/JonathanCastro07/Sella/config/SecurityConfig.java
@@ -62,7 +62,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200",
+                "https://sella-frontend-jonathan-castro-s-projects.vercel.app"
+                ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
