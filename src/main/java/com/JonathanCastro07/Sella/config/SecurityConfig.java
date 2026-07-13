12b2c81696676db728d0bb0b48e2a1c4a69d2f52
jonathan <jmanjarres238@gmail.com>
2026-07-13 16:33:57 -0500

    Corregir dominio de Vercel en CORS

--- src/main/java/com/JonathanCastro07/Sella/config/SecurityConfig.java
+++ src/main/java/com/JonathanCastro07/Sella/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("http://localhost:4200",
-                "https://sella-frontend-jonathan-castro-s-projects.vercel.app"
+                "https://sella-frontend.vercel.app/"
                 ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
